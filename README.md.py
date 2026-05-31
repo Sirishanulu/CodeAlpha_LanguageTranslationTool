a4b81098dc1aaea42d30b7014b496fafc00969d0
Sirishanulu <sirishanulu68@gmail.com>
2026-05-31 20:49:16 +0530

    Add files via upload

---
+++ README.md.py
@@ -0,0 +1,15 @@
+# CodeAlpha Language Translation Tool
+
+This project translates text from one language to another using Google Translator.
+
+## Features
+- Multiple language support
+- Easy-to-use interface
+- Instant translation
+
+## Technologies Used
+- Python
+- Streamlit
+- Deep Translator
+
+Developed as part of the CodeAlpha Artificial Intelligence Internship.
